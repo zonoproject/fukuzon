825f44b4d358f5d227ede8655882ec9b81d97a92
zonoproject <zonoproject@gmail.com>
2022-07-11 06:20:36 +0000

    04 create products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_045840) do
+ActiveRecord::Schema.define(version: 2022_07_11_051416) do
 
   create_table "categories", force: :cascade do |t|
     t.string "major_category_name"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2022_07_11_045840) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end

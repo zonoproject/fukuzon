2ef5bbe4100f409592a0e1df2c6fd92d466e5a6f
zonoproject <zonoproject@gmail.com>
2022-07-12 07:11:24 +0000

    16 add_user_info

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  before_action :set_user
+  def edit
+  end
+
+  def update
+    @user.update_without_password(user_params)
+    redirect_to mypage_users_url
+  end
+
+  def mypage
+  end
+  
+  def edit_address
+  end
+  
+  private
+  def set_user
+    @user = current_user
+  end
+  
+  def user_params
+    params.permit(:name, :email, :address, :phone, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,16 @@ Rails.application.routes.draw do
     get "login", :to => "users/sessions#new"
     delete "logout", :to => "users/sessions#destroy"
   end
+  
+  resource :users, only: [:edit, :update] do
+    collection do
+      get "mypage", :to => "users#mypage"
+      get "mypage/edit", :to => "users#edit"
+      get "mypage/address/edit", :to => "users#edit_address"
+      put "mypage", :to => "users#update"
+    end
+  end
+  
   resources :products do
     member do
       get :favorite

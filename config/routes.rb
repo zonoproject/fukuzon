3b749fcfadfc329eb5d186338b9d59098236de81
zonoproject <zonoproject@gmail.com>
2022-07-14 03:24:26 +0000

    22 create_top

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    root :to => "users/sessions#new"
+    root :to => "web#index"
     get "signup", :to => "users/registrations#new"
     get "verify", :to => "users/registrations#verify"
     get "login", :to => "users/sessions#new"

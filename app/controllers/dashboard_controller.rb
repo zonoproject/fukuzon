708c79c4b5275836098c60b531d85b0123195ad6
zonoproject <zonoproject@gmail.com>
2022-07-18 04:35:45 +0000

    2-06 add_admin

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+    before_action :authenticate_admin!
     layout 'dashboard/dashboard'
     def index
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get "dashboard", :to => "dashboard#index"
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
+ 
+  devise_scope :admin do
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    delete "dashboard/logout", :to => "admins/sessions#destroy"
+  end
+  
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
